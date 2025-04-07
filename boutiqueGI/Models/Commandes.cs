45ae974ef4806cf4879f75a7479e9f773551ed5e
robleh <roblehousein@gmail.com>
2025-04-07 19:58:15 +0300

    partie 1 du module commande terminé

--- boutiqueGI/Models/Commandes.cs
+++ boutiqueGI/Models/Commandes.cs
@@ -3,8 +3,16 @@
     public class Commandes
     {
         public Guid Id { get; set; }
-        public required List<Produits> Produits { get; set; }
-        public required Clients Client{ get; set; }
+        // liste des produits vendus
+        public  List<Produits>? Panier { get; set; }
+        // liste des produits en stock
+        public List<Produits>? Produits { get; set; }
+        // liste des clients déjà enregistrer
+        public IEnumerable<Clients>? Customers { get; set; }
+        public  Clients? Client { get; set; }
+        //nom du client qui a passé la commande
+        public  string? ClientName { get; set; }
         public DateTime DateCommande { get; set; } = DateTime.Now;
+        public Decimal Total_Price { get; set; } 
     }
 }
